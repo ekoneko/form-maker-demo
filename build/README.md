ed4a52a3a2410c1fe5e463b469e878aceb8e9e94
ekoneko <arzusyume@gmail.com>
2019-04-16 12:24:51 +0800

    chore: resolve unsafe deps

--- build/README.md
+++
@@ -1,1 +0,0 @@
-`update.sh` is a dev tool, use to sync `form-make` source code to `form-make-demo` dependencies directory.
--- build/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR=${PWD}
-FM_DIR=../form-maker
-cd $FM_DIR
-npm run compile
-rm -rf $DIR/node_modules/form-maker
-mkdir $DIR/node_modules/form-maker
-mv lib $DIR/node_modules/form-maker
-cp package.json $DIR/node_modules/form-maker
